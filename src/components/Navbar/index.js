08a8c93bac198c093f369522878dabcf68909af7
nikobagaric <bagaric@nulldesign.org>
2022-09-15 20:27:06 +0200

    sidebar stuff

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,31 @@
-import './index.css'
+import "./index.css";
 
 const Navbar = () => {
-    return (
-        <div className="navbar fixed rounded-b-full top-0 w-screen h-20 shadow-lg bg-main_red-100 z-10 overflow-x-hidden">
-            <div className="navbar-container flex justify-evenly">
-                <input type="checkbox" name="" id=""/>
-                <div className="hamburger">
-                    <span className="line line1"></span>
-                    <span className="line line2"></span>
-                    <span className="line line3"></span>
-                </div>
-                <h1 className="text-white font-semibold mt-4 text-4xl">Recipe Website</h1>
-            </div>
+  return (
+    <div>
+      <div className="navbar fixed rounded-b-full top-0 w-screen h-20 shadow-lg bg-main_red-100 z-10 overflow-x-hidden">
+        <div className="navbar-container flex justify-evenly">
+          <input type="checkbox" name="" id="" />
+          <div className="hamburger">
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </div>
+          <h1 className="text-white font-semibold mt-4 text-4xl">
+            Recipe Website
+          </h1>
         </div>
-    )
-}
+      </div>
+      <div className="sidebar">
+        <ul className="sidebar-list">
+          <a href=""><li className="sidebar-list-link"><p>Link</p></li></a>
+          <a href=""><li className="sidebar-list-link"><p>Link</p></li></a>
+          <a href=""><li className="sidebar-list-link"><p>Link</p></li></a>
+          <a href=""><li className="sidebar-list-link"><p>Link</p></li></a>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
