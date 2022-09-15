c1b7e4738e3b677366a79fb0391c8d22e8b1e809
nikobagaric <bagaric@nulldesign.org>
2022-09-15 13:51:52 +0200

    Styling for RecipePost

--- src/components/RecipePost/index.js
+++ src/components/RecipePost/index.js
@@ -1,21 +1,25 @@
-import './index.css'
+import "./index.css";
 
-const RecipePost = (props) => {
-    return (
-        <div className="recipe-post">
-            <img className="recipe-post-image" src={props.image}/>
-            <div className="recipe-body">
-                <div className="recipe-body-header">
-                    <a href={props.link}><h1 className="recipe-body-title">TEst{props.title}</h1></a>
-                    <h2 className="recipe-body-h2 time">{props.time_minutes}</h2>
-                    <h2 className="recipe-body-h2 cost">{props.cost}</h2>
-                </div>
-                <div className="recipe-body-description">
-                    <p className="recipe-body-paragraph">{props.description}</p>
-                </div>
-            </div>
+const RecipePost = ({ props }) => {
+  return (
+    <div className="recipe-container flex justify-center">
+      <div className="recipe-post">
+        <img className="recipe-post-image object-cover border-2 border-gray-300" src={props.image} />
+        <div className="recipe-body ml-8">
+          <div className="recipe-body-header">
+            <a href={props.link}>
+              <h1 className="recipe-body-title">{props.title}</h1>
+            </a>
+            <h2 className="recipe-body-h2 time">Date created: {props.time_minutes}</h2>
+            <h2 className="recipe-body-h2 cost">Estimated price: {props.cost}</h2>
+          </div>
+          <div className="recipe-body-description bg-gray-200 rounded-2xl p-3">
+            <p className="recipe-body-paragraph">{props.description}</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RecipePost
+export default RecipePost;
--- src/components/RecipePost/tempdata.js
+++ src/components/RecipePost/tempdata.js
@@ -1,12 +1,24 @@
 export default [
-    {
-        id: 1,
-        title: 'Sample Recipe',
-        description: 'sample description',
-        time_minutes: '3',
-        price: 3.5,
-        image: 'images/logo192.png',
-        link: 'https://google.com',
-        tags: ['tag A', 'tag B'],
-    }
-]
+  {
+    id: 1,
+    title: "Sample Recipe",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    time_minutes: "3hrs",
+    cost: 3.5,
+    image: "images/logo192.png",
+    link: "https://google.com",
+    tags: ["tag A", "tag B"],
+  },
+  {
+    id: 1,
+    title: "Sample Recipe",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    time_minutes: "3hrs",
+    cost: 3.5,
+    image: "images/logo192.png",
+    link: "https://google.com",
+    tags: ["tag A", "tag B"],
+  },
+];
