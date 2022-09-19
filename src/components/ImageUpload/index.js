fb4615262a95031fcd92924e7df024fe86b393bc
nikobagaric <niko@nulldesign.org>
2022-09-19 19:42:33 +0200

    Added PostRecipe form

---
+++ src/components/ImageUpload/index.js
@@ -0,0 +1,9 @@
+const ImageUpload = ({API_URL}) => {
+    return (
+        <div className="image-upload-container">
+            <button>amogus</button>
+        </div>
+    )
+}
+
+export default ImageUpload
--- src/pages/PostRecipe/index.js
+++ src/pages/PostRecipe/index.js
@@ -1,15 +1,81 @@
-const PostRecipe = () => {
+import './index.css'
+import { useState } from "react"
+
+import ImageUpload from '../../components/ImageUpload'
+
+const PostRecipe = ({ API_URL }) => {
+    const [formData, setFormData] = useState(
+        {
+            title: '',
+            description: '',
+            time_minutes: '',
+            cost: 0,
+            ingredients: [''],
+            tags: [''],
+            image: '',
+        }
+    )
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
     return (
-        <div className="recipe-post-container">
-            <div className="recipe-post">
-                <div className="recipe-post-form form">
-                    <input className="title-form"></input>
-                    <input className="description-form"></input>
-                    <input className="ingredients-form"></input>
-                    <input className="time_minutes-form"></input>
-                    <input className="cost-form"></input>
-                    <input className="tags-form"></input>
+        <div className="recipe-upload-container">
+            <div className="recipe-upload">
+                <div className="recipe-upload-form post-form">
+                    <label className="title-label post-form-label" htmlFor="title">Recipe title:</label>
+                    <input className="title-form post-form-input"
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        onChange={handleChange}
+                        value={formData.title}></input>
+                    <label className="title-label post-form-label" htmlFor="title">Recipe description:</label>
+                    <textarea className="description-form post-form-input"
+                        type="text"
+                        name="description"
+                        placeholder="Description"
+                        onChange={handleChange}
+                        value={formData.description}></textarea>
+                    <label className="title-label post-form-label" htmlFor="title">Recipe ingredients:</label>
+                    <input className="ingredients-form post-form-input"
+                        type="text"
+                        name="ingredients"
+                        placeholder="Ingredients"
+                        onChange={handleChange}
+                        value={formData.ingredients}></input>
+                    <label className="title-label post-form-label" htmlFor="title">Time required:</label>
+                    <input className="time_minutes-form post-form-input"
+                        type="text"
+                        name="time_minutes"
+                        placeholder="Time in minutes"
+                        onChange={handleChange}
+                        value={formData.time_minutes}></input>
+                    <label className="title-label post-form-label" htmlFor="title">Estimated cost:</label>
+                    <input className="cost-form post-form-input"
+                        type="text"
+                        name="cost"
+                        placeholder="Cost"
+                        onChange={handleChange}
+                        value={formData.cost}></input>
+                    <label className="title-label post-form-label" htmlFor="title">Recipe tags:</label>
+                    <input className="tags-form post-form-input"
+                        type="text"
+                        name="tags"
+                        placeholder="Tags"
+                        onChange={handleChange}
+                        value={formData.tags}></input>
                 </div>
+            </div>
+            <div className='default-container'>
+                <ImageUpload API_URL={API_URL}/>
             </div>
         </div>
     )
